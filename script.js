c71c20e308c2c3185e14b927276786ff75df8885
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-19 00:02:35 +0300

    add updateCell function and add click event listeners to all cells

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const board = (() => {
-    const cellArray=["X", "O", "X", "X", "O", "X", "O", "X", "O"];
-    const populateArray=()=>{
-        for(let i=1; i<=9; i++){
+    const cellArray = ["X", "O", "X", "X", "O", "X", "O", "X", "O"];
+    const populateArray = () => {
+        for (let i = 1; i <= 9; i++) {
             cellArray.push(document.getElementById(i))
-        }  }
-    const displayArray=()=>{
-        for(let i=1; i<10; i++){
-            let temp=document.getElementById(i)
-            console.log(temp)
-            temp.textContent=cellArray[i]
         }
     }
+    const updateCell = (index, element) => {
+        let temp = document.getElementById(index)
+        temp.textContent = element
+    }
+    const displayArray = () => {
+        for (let i = 1; i < 10; i++) {
+            updateCell(i, cellArray[i])
+        }
+    }
+    document.querySelectorAll(".cell").forEach(cell =>{
+        cell.addEventListener('click', ()=>{
+            console.log(cell.id)
+        })
+    })
     return {
         cellArray,
         displayArray
     };
-  })();
+})();
 
-  console.log(board.cellArray)
-  board.displayArray()
+console.log(board.cellArray)
+board.displayArray()
