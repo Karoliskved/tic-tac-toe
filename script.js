373ecdc8bd2f562b0cd1ad03300b1001a7b0421b
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-19 20:03:08 +0300

    add function which checks win conditions, fix several bugs

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const player = (name, element) => {
     const getName = () => name;
     const getElement = () => element;
+    const setName = (newName) => {
+        name = newName;
+    }
     return {
         getName,
-        getElement
+        getElement,
+        setName
     }
 }
 const board = (() => {
-    const cellArray = ["", "", "", "", "", "", "", "", "", ""];
+    let cellArray = ["", "", "", "", "", "", "", "", ""];
     const populateArray = () => {
-        for (let i = 1; i <= 9; i++) {
+        for (let i = 0; i < 9; i++) {
             cellArray.push(document.getElementById(i))
         }
     }
@@ -18,48 +22,97 @@ const board = (() => {
         temp.textContent = element
     }
     const displayArray = () => {
-        for (let i = 1; i < 10; i++) {
-            updateCell(i, cellArray[i])
+        for (let i = 0; i < 9; i++) {
+            console.log(cellArray)
+            console.log(board.cellArray)
+            updateCell(i, board.cellArray[i])
         }
     }
+    const fillArray = () => {
+        board.cellArray = Array(9).fill("")
+        console.log(cellArray)
+    }
+
+
     return {
         cellArray,
-        displayArray
+        displayArray, fillArray
     };
 })();
 const gameFlow = (() => {
-    let status = false;
+    let status = true;
     const player1 = player('John', 'X')
     const player2 = player('Bob', 'O')
+    const getStatus = () => status;
     const changeStatus = () => {
         status = !status;
     }
+    const handleWin = () => {
+        if (!status) {
+            alert(player1.getName() + " wins!")
+        }
+        else
+            alert(player2.getName() + " wins!")
+    }
+    const checkForWin = () => {
+        console.log("testing")
+        if (board.cellArray[0] == board.cellArray[1] && board.cellArray[0] == board.cellArray[2] && board.cellArray[0] !== "")
+            handleWin();
+        else if (board.cellArray[3] == board.cellArray[4] && board.cellArray[3] == board.cellArray[5] && board.cellArray[3] !== "")
+            handleWin();
+        else if (board.cellArray[6] == board.cellArray[7] && board.cellArray[7] == board.cellArray[8] && board.cellArray[7] !== "")
+            handleWin();
+        else if (board.cellArray[0] == board.cellArray[3] && board.cellArray[0] == board.cellArray[6] && board.cellArray[0] !== "")
+            handleWin();
+        else if (board.cellArray[1] == board.cellArray[4] && board.cellArray[1] == board.cellArray[7] && board.cellArray[1] !== "")
+            handleWin();
+        else if (board.cellArray[2] == board.cellArray[5] && board.cellArray[2] == board.cellArray[8] && board.cellArray[2] !== "")
+            handleWin();
+        else if (board.cellArray[0] == board.cellArray[4] && board.cellArray[0] == board.cellArray[8] && board.cellArray[0] !== "")
+            handleWin();
+        else if (board.cellArray[2] == board.cellArray[4] && board.cellArray[2] == board.cellArray[6] && board.cellArray[2] !== "")
+            handleWin();
+    }
+
     const handlePlay = (status, cell) => {
-        console.log('halp me')
+        //console.log('halp me')
         let element
         if (status)
-            element = player1.getElement()
+            element = player1.getElement();
         else
             element = player2.getElement()
-        if (board.cellArray[cell.id] === "") {
-            board.cellArray[cell.id] = element
-            gameFlow.changeStatus()
-            board.displayArray()
+        if (board.cellArray[cell.id] == "") {
+            board.cellArray[cell.id] = element;
+            gameFlow.changeStatus();
+            board.displayArray();
+            gameFlow.checkForWin();
         }
     }
-    const getStatus = () => status;
-    return {
-        changeStatus, getStatus, handlePlay
+    const startRound = (name1, name2) => {
+        status=true;
+        player1.setName(name1)
+        player2.setName(name2)
+        /*console.log(player1.getName())
+        console.log(player2.getName())*/
+        board.fillArray()
+        board.displayArray()
 
+    }
+    return {
+        changeStatus, getStatus, handlePlay, startRound, checkForWin
     };
 })();
 
 document.querySelectorAll(".cell").forEach(cell => {
     cell.addEventListener('click', () => {
-        console.log("why")
+         console.log("why")
         gameFlow.handlePlay(gameFlow.getStatus(), cell)
     })
 })
-player1 = player('test', 'x')
-console.log(board.cellArray)
+document.querySelector(".start").addEventListener('click', () => {
+    console.log("test")
+    gameFlow.startRound(document.getElementById("p1").value, document.getElementById("p2").value)
+
+})
+//console.log(board.cellArray)
 board.displayArray()
