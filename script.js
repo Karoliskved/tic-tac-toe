b732fc673fdaea09eed4433bc55b50a8c339b57d
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-19 16:35:38 +0300

    add a function which changes the display of a cell on click

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const player = (name, element)=>{
+    const getName=()=> name;
+    const getElement=()=> element;
+    return{
+        getName, 
+        getElement
+    }
+}
 const board = (() => {
     const cellArray = ["X", "O", "X", "X", "O", "X", "O", "X", "O"];
     const populateArray = () => {
@@ -14,16 +22,42 @@ const board = (() => {
             updateCell(i, cellArray[i])
         }
     }
-    document.querySelectorAll(".cell").forEach(cell =>{
-        cell.addEventListener('click', ()=>{
-            console.log(cell.id)
-        })
-    })
     return {
         cellArray,
         displayArray
     };
 })();
+const gameFlow = (() => {
+    let status=false;
+    const player1=player('John', 'X')
+    const player2=player('Bob', 'O')
+    const changeStatus=()=>{ 
+        status=!status;
+    }
+    const handlePlay=(status, cell)=>{
+        console.log('halp me')
+        let element
+        if(status)
+        element=player1.getElement()
+        else
+        element=player2.getElement()
+        board.cellArray[cell.id]=element
+        gameFlow.changeStatus()
+        board.displayArray()
+    }
+    const getStatus=()=> status;
+    return {
+        changeStatus, getStatus, handlePlay
 
+    };
+})();
+
+document.querySelectorAll(".cell").forEach(cell =>{
+    cell.addEventListener('click', ()=>{
+        console.log("why")
+        gameFlow.handlePlay(gameFlow.getStatus(), cell)
+    })
+})
+player1=player('test', 'x')
 console.log(board.cellArray)
 board.displayArray()
